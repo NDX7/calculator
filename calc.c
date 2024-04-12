d2a30f037005848590a0be196c5cf7d3132ae83b
Nidhin Xavier <132472527+NDX7@users.noreply.github.com>
2024-04-12 19:49:30 +0530

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int sum()
+
+int main()
+{
+    sum()
+    return 0;
+}
+int sum()
+{   int a,b;
+    printf("enter number 1 to innput");
+    scanf("%d",&a);
+    printf("enter number 2 to innput");
+    scanf("%d",&b);
+    sum=a+b;
+    printf("%d",sum);
+
+}
